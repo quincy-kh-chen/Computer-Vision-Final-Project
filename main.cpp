4a87c0b0e744348af2c8fa1af9b3773066f2a7fa
Wei-Hsin Chou <weihsinc@andrew.cmu.edu>
2016-04-23 22:02:24 -0400

    Parameter tuning. PSR threshold affects a lot

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ bool select_flag = 0;
 bool draw_flag=0;
 
 //Matrix to store each frame of the webcam feed
-Mat CameraFeed;
+Mat cameraFeed;
 Mat frame;
 Mat box; //box - the part of the image in the bounding box
 
@@ -55,28 +55,28 @@ void mouseHandler(int event, int x, int y, int flags, void *param)
         // /* left button clicked. ROI selection begins */
         point1 = Point(x, y);
         drag = 1;
-        cout<<"mouse down"<<endl;
+        cout << "mouse down" << endl;
     }
     if (event == CV_EVENT_MOUSEMOVE && drag)
     {
-        draw_flag=1;
+        draw_flag = 1;
         point2 = Point(x, y);
     }
     if (event == CV_EVENT_LBUTTONUP && drag)
     {
         point2 = Point(x, y);
-        rect_template = Rect(point1.x, point1.y, x - point1.x, y - point1.y);//should not include the red box lines
+        rect_template = Rect(point1.x, point1.y, x - point1.x, y - point1.y); //should not include the red box lines
         drag = 0;
         Mat img2;
-        CameraFeed.copyTo(img2);
+        cameraFeed.copyTo(img2);
         box = img2(rect_template);
-        cout<<"mouse up"<<endl;
+        cout << "mouse up" << endl;
     }
     if (event == CV_EVENT_LBUTTONUP)
     {
         select_flag = 1;
         drag = 0;
-        draw_flag=0;
+        draw_flag = 0;
     }
 }
 string intToString(int number)
@@ -88,10 +88,8 @@ string intToString(int number)
 void ReadGroundTruth(const string pathToGroundTruth, Rect &rect_template)
 {
     //Each row in the ground-truth files: (x, y, box-width, box-height).
-    int x;
-    int y;
-    int box_width;
-    int box_height;
+    int x, y;
+    int box_width, box_height;
     
 //    const char * c = pathToGroundTruth.c_str();
 //    FILE *fp = fopen(c, "r");
@@ -103,11 +101,11 @@ void ReadGroundTruth(const string pathToGroundTruth, Rect &rect_template)
     // while(!infile.eof) 
 
 
-        std::getline(infile,line); // Saves the line in line
+        std::getline(infile, line); // Saves the line in line
         //remove comma
         
         std::string::size_type n = 0;
-        while ( ( n = line.find( ',', n ) ) != std::string::npos )
+        while ( (n = line.find(',', n)) != std::string::npos )
         {
             line.replace( n, 1, 1, ' ' );
             n++;
@@ -119,9 +117,9 @@ void ReadGroundTruth(const string pathToGroundTruth, Rect &rect_template)
         int c = in.peek();  // peek character
         if(isdigit(c)|| c=='-')
         {
-            in >> x >> y >> box_width >>box_height;
+            in >> x >> y >> box_width >> box_height;
 
-            cout<<"x="<<x<<" y="<<y<<" w="<<box_width<<" h="<<box_height<<endl;
+            cout<< "x = " << x << "y = " << y << "w = " << box_width << "h = " << box_height << endl;
         }
     
     rect_template = Rect(x, y, box_width, box_height);
@@ -130,126 +128,151 @@ void ReadGroundTruth(const string pathToGroundTruth, Rect &rect_template)
 
 int main(int argc, char* argv[])
 {    
-    bool ImageSequence=1;
+    bool ImageSequence = 0;
 
-    if(ImageSequence)
+    if (ImageSequence)
     {
-        string pathToData("/Users/Hsin/Desktop/CVproject/TestVideo/MotorRolling");
-        string pathToImg=pathToData+"/img/%04d.jpg";
-        string pathToGroundTruth=pathToData+"/groundtruth_rect.txt";
+        string pathToData("/Users/WeiHsin/Desktop/TestVideo");
+        string pathToVideo("/Matrix");
+        string pathToImg = pathToData+pathToVideo + "/img/%04d.jpg";
+        string pathToGroundTruth = pathToData+pathToVideo + "/groundtruth_rect.txt";
         ReadGroundTruth(pathToGroundTruth, rect_template);
         capture.open(pathToImg);
-        if(!capture.isOpened())
+        if (!capture.isOpened())
         {
-            cout<<"Can't not read image sequence"<<endl;
+            cout << "Can't not read image sequence" << endl;
             getchar();
             return -1;
         }
         //set height and width of capture frame
-        capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);//FRAME_WIDTH = 640;
-        capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);//FRAME_HEIGHT = 480;
-        capture.set(CV_CAP_PROP_FPS,2);
+        capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH); //FRAME_WIDTH = 640;
+        capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);//FRAME_HEIGHT = 480;
+        capture.set(CV_CAP_PROP_FPS, 2);
 
 //        ReadGroundTruth(pathToGroundTruth, rect_template);
         //store image to matrix //cameraFeed is a matrix
-        capture.read(CameraFeed);
-        cvtColor(CameraFeed,frame,CV_RGB2GRAY);
+        capture.read(cameraFeed);
+        if (cameraFeed.channels() == 3)
+        {
+            cvtColor(cameraFeed, frame, CV_RGB2GRAY);
+        
+        }
+        if (cameraFeed.channels() == 1)
+        {
+            frame = cameraFeed;
+            
+        }
         // rect_template = Rect(x, y, box_width, box_height);
 
         Tracker track_mosse(frame, rect_template);
         Trackers.push_back(track_mosse);
 
-        imshow("Tracking",CameraFeed);//original pic
-        select_flag=1;
-        waitKeyTime=10;
+        imshow("Tracking", cameraFeed); //original pic
+        select_flag = 1;
+        waitKeyTime = 10;
     }
 
-    else if(!ImageSequence)
+    else if (!ImageSequence)
     {
         //open capture object at location zero (default location for webcam)
         capture.open(0);
-        if(!capture.isOpened())
+        if (!capture.isOpened())
         {
-            cout<<"Webcam can't be opened"<<endl;
+            cout << "Webcam can't be opened" << endl;
             getchar();
             return -1;
         }
-        capture.set(CV_CAP_PROP_FPS,30);
-        capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);//FRAME_WIDTH = 640;
-        capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);//FRAME_HEIGHT = 480;
+        capture.set(CV_CAP_PROP_FPS, 30);
+        capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH); //FRAME_WIDTH = 640;
+        capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT); //FRAME_HEIGHT = 480;
 
         Tracker track_mosse;
         Trackers.push_back(track_mosse);
         //store image to matrix //cameraFeed is a matrix
-        capture.read(CameraFeed);
-        imshow("Tracking",CameraFeed);//original pic
-        select_flag=0;
-        waitKeyTime=1;
+        capture.read(cameraFeed);
+        imshow("Tracking", cameraFeed);//original pic
+        select_flag = 0;
+        waitKeyTime = 1;
     }
 
 
     
     
  Start: 
-    while(1)
+    while (1)
     {
-        if(!ImageSequence)
+        if (!ImageSequence)
         {
             setMouseCallback("Tracking", mouseHandler, NULL);
         }
         clock_t begin_time = clock();
 
-        capture.read(CameraFeed);
-        cvtColor(CameraFeed,frame,CV_RGB2GRAY);
-
-        if(frame.size().height==0)
+        capture.read(cameraFeed);
+        if (cameraFeed.channels() == 3)
         {
-            cout<<"image sequence end"<<endl;
+            cvtColor(cameraFeed, frame, CV_RGB2GRAY);
+            
+        }
+        if (cameraFeed.channels() == 1)
+        {
+            frame = cameraFeed;
+            
+        }
+
+        if (frame.size().height == 0)
+        {
+            cout << "image sequence end" << endl;
             getchar();
             return -1;
         }
 
-        double CameraTime=float( clock () - begin_time )*1000 /  CLOCKS_PER_SEC; 
-        // cout << "Camera taken: "<<CameraTime<<" ms, "<<" ";
+        double CameraTime = float(clock() - begin_time)*1000 / CLOCKS_PER_SEC;
+        //cout << "Camera taken: "<<CameraTime<<" ms, "<<" ";
 
-////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
         begin_time = clock();
 
-        for(int i=0;i<Trackers.size();i++)
+        for (int i = 0; i < Trackers.size(); i++)
         {
-            Trackers[i].Run(CameraFeed,frame,rect_template,select_flag,point1,point2, draw_flag);
-            putText(CameraFeed,"w"+intToString(i+1)+":"+intToString(Trackers[i].tracker_mosse.w),Point(FRAME_WIDTH-160,50+i*20),5,1,Scalar(255,0,0),1);
-            putText(CameraFeed,"  h"+intToString(i+1)+":"+intToString(Trackers[i].tracker_mosse.h),Point(FRAME_WIDTH-100,50+i*20),5,1,Scalar(255,0,0),1);
+            Trackers[i].Run(cameraFeed, frame, rect_template, select_flag, point1, point2, draw_flag);
+            
+            putText(cameraFeed, "w" + intToString(i+1) + ": " + intToString(Trackers[i].tracker_mosse.w),
+                    Point(FRAME_WIDTH-160, 50+i*20), 5, 1, Scalar(255, 0, 0), 1);
+            
+            putText(cameraFeed, "h" + intToString(i+1) + ": " + intToString(Trackers[i].tracker_mosse.h),
+                    Point(FRAME_WIDTH-100, 50+i*20), 5, 1, Scalar(255, 0, 0), 1);
 
         }
 
-        float ElapsedTime=float( clock () - begin_time )*1000 /  CLOCKS_PER_SEC; 
+        float ElapsedTime = float(clock() - begin_time)*1000 / CLOCKS_PER_SEC;
         // cout << "Time taken: "<<ElapsedTime<<" ms, "<<"fps : "<<int(1000/ElapsedTime)<<endl;
         // putText(CameraFeed,"w:"+intToString(tracker.w),Point(FRAME_WIDTH-140,50),5,1,Scalar(255,0,0),1);
         // putText(CameraFeed," h:"+intToString(tracker.h),Point(FRAME_WIDTH-80,50),5,1,Scalar(255,0,0),1);
-        if(select_flag==0)
+        if (select_flag==0)
         {
-            putText(CameraFeed," Selecting Template!",Point(0,20),5,1,CV_RGB(0,0,255),2);
+            putText(cameraFeed, " Selecting Template!", Point(0, 20), 5, 1, CV_RGB(0, 0, 255), 2);
         }
        
-        putText(CameraFeed,"fps: "+intToString(int(1000/ElapsedTime)),Point(FRAME_WIDTH-160,20),5,1,Scalar(255,0,0),1);
-        imshow("Tracking",CameraFeed);
-////////////////////////////////////////////////////////////
+        putText(cameraFeed, "fps: " + intToString(int(1000/ElapsedTime)), Point(FRAME_WIDTH-160, 20),
+                5, 1, Scalar(255, 0, 0), 1);
+        imshow("Tracking",cameraFeed);
+/////////////////////////////////////////////////////////////////////////////////////////////////
         
-        if(waitKey(waitKeyTime)==13)
+        if (waitKey(waitKeyTime) == 13)
         {
-            cout<<"New Object!"<<endl;
-            select_flag=0;
+            cout << "New Object!" << endl;
+            select_flag = 0;
             Tracker track_mosse;
             Trackers.push_back(track_mosse);
         }        
-        if(waitKey(waitKeyTime)==32)
+        
+        if (waitKey(waitKeyTime) == 32)
         {
-            select_flag=0;
+            select_flag = 0;
             Trackers.clear();
             goto Start;
         }
-        if(waitKey(waitKeyTime)==27)
+        if (waitKey(waitKeyTime) == 27)
         {
             break;
         }
